fad12d0b6f423cfc46fb2c912f62966ae382a796
AzizBooker <bookeraziz@gmail.com>
2021-06-24 14:57:27 -0400

    added mesh to scene

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var camera=new THREE.PerspectiveCamera(
     0.1,
     1000
 )
+camera.position.z=5
 var renderer=new THREE.WebGLRenderer({antialias:true})
 
 renderer.setClearColor('#e5e5e5');
@@ -16,10 +17,32 @@ document.body.appendChild(renderer.domElement)
 window.addEventListener('resize',()=>{
     renderer.setSize(window.innerWidth,window.innerHeight);
     camera.aspect(window.innerWidth/ window.innerHeight)
+
+    camera.updateProjectMatrix();
 })
 
 
-var geometry=THREE.
+var geometry=new THREE.BoxGeometry(1,1,1)
+var material=new THREE.MeshLambertMaterial({color:0xFFCC00})
+//var material=new THREE.MeshToonMaterial({color:0xFFCC00})
+var mesh=new THREE.Mesh(geometry,material)
+
+mesh.position.x=-2
+mesh.position.y=-2
 
 
-renderer.render(scene,camera)
+scene.add(mesh)
+
+var light=new THREE.PointLight(0xFFFFFF,1,500)
+light.position.set(10,0,25);
+scene.add(light)
+
+var render=function(){
+    requestAnimationFrame(render)
+
+    
+
+    renderer.render(scene,camera)
+}
+
+render()
